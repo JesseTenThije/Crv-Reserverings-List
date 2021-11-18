c49ae15f80add113d4d1277adabd666cea0975de
[Jesse] <[jtenthije04@student.rocvantwente.nl]>
2021-11-18 12:13:22 +0100

    Added Solution

---
+++ CCSB/CCSB/Models/Reserveringen.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCSB.Models
+{
+    public class Reserveringen
+    {
+        [Key]
+        public int? Id { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "{0} is een verplicht veld.")]
+        public DateTime? StartDatum { get; set; }
+
+        public int CrvId { get; set; }
+
+        [DisplayName("Voertuig")]
+        public Crv Crv { get; set; }
+    }
+}
